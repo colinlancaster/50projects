9e4627b987548b4a20a225d56f8add232fd324d4
Colin Lancaster <Colinl@biz.oregon.gov>
2021-01-11 09:19:40 -0800

    finished up video

--- 17-Movie-App/script.js
+++ 17-Movie-App/script.js
@@ -4,6 +4,7 @@ const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 // Use a single double quote
 const SEARCH_API = 'http://api.themoviedb.org/3/search/movie?api_key=47a505a0f534f4448fcae94d59485023&query="';
 
+const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
 
@@ -13,7 +14,8 @@ getMovies(API_URL);
 async function getMovies(url) {
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data.results);
+
+    showMovies(data.results);
 }
 
 form.addEventListener('submit', (e) => {
@@ -28,4 +30,39 @@ form.addEventListener('submit', (e) => {
     } else {
         window.location.reload();
     }
-})
+});
+
+function showMovies(movies) {
+    main.innerHTML = '';
+
+    movies.forEach((movie) => {
+        const { title, poster_path, vote_average, overview} = movie;
+
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+
+        movieEl.innerHTML = `
+            <img src="${IMG_PATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+
+            <div class="overview">
+                <h3>Overview</h3>
+                ${overview}
+            </div>`
+
+        main.appendChild(movieEl);
+    });
+}
+
+function getClassByRate(vote) {
+    if (vote >= 8) {
+        return 'green';
+    } else if (vote >= 5) {
+        return 'orange';
+    } else {
+        return 'red';
+    }
+}
