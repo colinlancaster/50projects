7763db20ebb7b0877331e140d264762e08791f93
Colin Lancaster <Colinl@biz.oregon.gov>
2021-01-22 13:24:13 -0800

    halfway through double sliders

--- 26-Sticky-Navbar/script.js
+++ 26-Sticky-Navbar/script.js
@@ -2,6 +2,8 @@ const nav = document.querySelector('.nav');
 window.addEventListener('scroll', fixNav);
 
 function fixNav() {
+    console.log("I have scrolled to this position: " + window.scrollY);
+    console.log("This is the current height of the nav bar and its border + 150: " + (nav.offsetHeight + 150));
     if (window.scrollY > nav.offsetHeight + 150) {
         nav.classList.add('active');
     } else {
